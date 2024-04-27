addb1551188f2ba3fad74a7213bada1a3d57d12e
Christine Seungmin Roh <smroh@bu.edu>
2024-04-26 22:01:03 -0400

    First Three Page Code

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,30 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+function changeWhiteBackgroundColor() {
+    document.getElementsByClassName("dot")[0].style.backgroundColor = "#FDFEE3";
+    document.getElementById("comment").innerHTML = "White tea is good for spring and summer.";
+    document.getElementById('arrows').style.display = "block";
+ }
+
+function changeYellowBackgroundColor() {
+    document.getElementsByClassName("dot")[0].style.backgroundColor = "#FFD84D";
+    document.getElementById("comment").innerHTML = "Yellow tea is good for spring and summer.";
+    document.getElementById('arrows').style.display = "block";
+
+ }
+
+ function changeRooibosBackgroundColor() {
+    document.getElementsByClassName("dot")[0].style.backgroundColor = "#D31F1F";
+    document.getElementById("comment").innerHTML = "Rooibos tea is good for fall and winter.";
+    document.getElementById('arrows').style.display = "block";
+
+ }
+
+ function changeBlackBackgroundColor() {
+    document.getElementsByClassName("dot")[0].style.backgroundColor = "#301705";
+    document.getElementById("comment").innerHTML = "Black tea is good for fall and winter.";
+    document.getElementById('arrows').style.display = "block";
+
+ }
